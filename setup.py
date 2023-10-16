37c4c084d79488e8eb5df5752172f070e4574e58
Damien LaRocque <phicoltan@gmail.com>
2023-10-16 16:33:46 -0400

    Rename package; fill package metadata

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import find_packages, setup
 
-package_name = 'LSM9DS0_ros'
+package_name = "lsm9ds0_ros"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.1",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools", "adafruit-circuitpython-lsm9ds0"],
     zip_safe=True,
-    maintainer='norlab',
-    maintainer_email='phicoltan@gmail.com',
-    description='TODO: Package description',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="Damien LaRocque",
+    maintainer_email="phicoltan@gmail.com",
+    description="ROS 2 driver for the Adafruit LSM9DS0 IMU",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "lsm9ds0_node = lsm9ds0_ros.lsm9ds0_node:main",
         ],
     },
 )
